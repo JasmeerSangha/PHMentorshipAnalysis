43f78697bceb7f4977b4c64c5c843228f9882400
JasmeerSangha <61358666+JasmeerSangha@users.noreply.github.com>
2020-04-10 11:12:34 -0400

    Started  querying data.

--- Data/schema.sql
+++ Data/schema.sql
@@ -36,12 +36,12 @@ CREATE TABLE salaries (
 );
 
 CREATE TABLE titles (
+	id SERIAL PRIMARY KEY,
 	emp_no INT NOT NULL,
 	title VARCHAR NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  	PRIMARY KEY (emp_no)
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
 
 CREATE TABLE dept_emp(
@@ -51,5 +51,5 @@ CREATE TABLE dept_emp(
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-	PRIMARY KEY (emp_no)
+	PRIMARY KEY (emp_no, dept_no)
 );
