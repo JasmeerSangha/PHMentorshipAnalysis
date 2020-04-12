30f2022d4a88836ebbe7cbbcbc4d971b8332a34a
JasmeerSangha <61358666+JasmeerSangha@users.noreply.github.com>
2020-04-12 14:00:14 -0400

    debug.

--- Challenge/challenge.sql
+++ Challenge/challenge.sql
@@ -1,41 +1,30 @@
--- Link employees, titles and salaries
+-- Find positions and salaries of all current employees
 
-SELECT e.emp_no, e.first_name, e.last_name, 
+SELECT e.emp_no, e.first_name, e.last_name,
 	t.title, t.from_date,
 	s.salary
 INTO emp_title
 FROM employees as e
 INNER JOIN titles as t
-ON (e.emp_no = t.emp_no)
+	ON (e.emp_no = t.emp_no)
 INNER JOIN salaries as s
-ON (e.emp_no = s.emp_no)
+	ON (e.emp_no = s.emp_no)
 INNER JOIN dept_emp as de
-ON (e.emp_no = de.emp_no);
+	ON (e.emp_no = de.emp_no)
+WHERE (de.to_date = '9999-01-01')
+	AND (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+	AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+	 
 
---Finding latest titles
+-- Ensure these are most recent titles
+SELECT * INTO current_retiring_emp
+FROM (SELECT *, ROW_NUMBER() OVER (PARTITION BY emp_no ORDER BY from_date DESC) AS title_count
+	 From emp_title) withtitle_count
+	 where title_count =1;
 
-SELECT * 
-INTO ordered_emp_title
-FROM emp_title ORDER BY from_date DESC;
 
-SELECT DISTINCT ON (emp_no) * 
-INTO latest_titles
-FROM ordered_emp_title;
-
-SELECT title, count(emp_no) 
-INTO title_counts
-FROM latest_titles
+-- Count the number of retiring employees in each position
+SELECT title as "Title", count(current_retiring_emp) as "Number of Employees" 
+INTO retiring_count
+FROM current_retiring_emp
 GROUP BY title;
-
--- Find potential mentors
-
-SELECT l.emp_no, l.first_name, l.last_name, l.title, l.from_date,
-	t.to_date
-INTO mentors
-FROM latest_titles as l
-INNER JOIN  employees as e
-ON (l.emp_no = e.emp_no)
-INNER JOIN titles as t
-ON (l.emp_no, l.title)=(t.emp_no, t.title)
-WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31');  
-	 AND (t.to_date = '9999-01-01');
